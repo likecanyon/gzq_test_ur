d28150371fd12cb679920c7c3614ad0f78a21f3d
likecanyon <1174578375@qq.com>
2022-07-03 11:04:42 +0800

    计算完成

--- GravityCompensation.cpp
+++ GravityCompensation.cpp
@@ -29,7 +29,7 @@ Eigen::Matrix4d GetHomoTransMatrix(std::vector<double> I)
     return T;
 }
 
-//通过x y z Rx Ry Rz 获得旋转变换矩阵R 
+//通过x y z Rx Ry Rz 获得旋转变换矩阵R
 Eigen::Matrix4d GetRotationMatrix(std::vector<double> I)
 {
     Eigen::Vector3d K = {I[3], I[4], I[5]};
@@ -42,19 +42,43 @@ Eigen::Matrix4d GetRotationMatrix(std::vector<double> I)
     return R;
 }
 
-
 /*
 将重力投射到传感器坐标系
  param1 G 重力在m系下的表达；
  R_m_t m系相对于t系的位姿；
+ R_s_t
  P 机器人质心位置在s系下的表示。
 */
 std::vector<double> ForceTrans(std::vector<double> G, Eigen::Matrix3d R_m_t, std::vector<double> P)
 {
-    std::vector<double> F(6, 0.0);//重力在传感器坐标系的表达
+    std::vector<double> F(6, 0.0); //重力在传感器坐标系的表达
 
+    Eigen::Vector3d Ft;
+    Eigen::VectorXd F_before;
+    Eigen::VectorXd F_After;
+    Eigen::Vector3d G_w = {G[0], G[1], G[2]};
+    Eigen::MatrixXd TransForce;
+    Eigen::Matrix3d R_s_t;
+    Eigen::Matrix3d SP;
+    SP << 0, -P[2], P[1],
+        P[2], 0, -P[0],
+        -P[1], P[0], 0;
 
+    R_s_t.setIdentity();
+    TransForce.setIdentity();
 
+    Ft = R_m_t * G_w; //重力在t系中的表达
+    F_before[0] = Ft[0], F_before[1] = Ft[1], F_before[2] = Ft[2];
+    F_before[3] = 0, F_before[4] = 0, F_before[5] = 0;
+    TransForce.block(0, 0, 3, 3) = R_s_t;
+    TransForce.block(3, 3, 3, 3) = R_s_t;
+    TransForce.block(3, 0, 3, 3) = -SP * R_s_t;
+    F_After = TransForce.inverse() * F_before;
+
+    for (int i = 0; i < 6; i++)
+    {
+        F[i] = F_After[i];
+    }
 
     return F;
 }
@@ -65,9 +89,12 @@ int main(int argc, char *argv[])
     ur_rtde::RTDEReceiveInterface rtde_receive("192.168.3.101");
 
     //重力
-    double m=1.0;
-    double g=9.8;
-    std::vector<double> G{0.0, 0.0, -m*g};//重力在m系下的表示
+    double m = 1.0;
+    double g = 9.8;
+    std::vector<double> G{0.0, 0.0, -m * g}; //重力在m系下的表示
+    std::vector<double> FGS(6, 0.0);
+    //质心位置
+    std::vector<double> Position;
 
     //传感器读数
     std::vector<double> FS_Source(6, 0.0);
@@ -101,10 +128,15 @@ int main(int argc, char *argv[])
     R_s_w = GetRotationMatrix(TCPPoseVector);
 
     //计算各个坐标系的相对姿态关系
-    R_m_w.setIdentity(); // m坐标系与机器人基坐标系姿态平行；
-    R_t_s.setIdentity(); // t坐标系与机器人传感器坐标系平行；
-    R_m_t=R_s_w.inverse();//R_m_t=R_w_s
-
+    R_m_w.setIdentity();     // m坐标系与机器人基坐标系姿态平行；
+    R_t_s.setIdentity();     // t坐标系与机器人传感器坐标系平行；
+    R_m_t = R_s_w.inverse(); // R_m_t=R_w_s
+    FS_Source = rtde_receive.getActualTCPForce();
+    FGS = ForceTrans(G, R_m_t, Position);
+    for (int i = 0; i < 6; i++)
+    {
+        FS_AfterCompensation[i] = FS_Source[i] - FGS[i];
+    }
 
     return 0;
 }
