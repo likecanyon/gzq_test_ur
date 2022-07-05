d8ac3f722d692e42977c8a705d94f02dc8499807
likecanyon <1174578375@qq.com>
2022-07-05 21:23:47 +0800

    【添加机器人初始位姿】

--- GetSomeData.cpp
+++ GetSomeData.cpp
@@ -1,5 +1,16 @@
+
 #include <ur_rtde/rtde_control_interface.h>
 #include <ur_rtde/rtde_receive_interface.h>
+#include <ur_rtde/rtde_io_interface.h>
+
+#include <iostream>
+
+#include <thread>
+
+#include <chrono>
+
+#include <Eigen/Dense>
+#include <cmath>
 
 using namespace ur_rtde;
 
@@ -7,10 +18,14 @@ int main(int argc, char *argv[])
 {
   RTDEControlInterface rtde_control("192.168.3.101");
   RTDEReceiveInterface rtde_receive("192.168.3.101");
+  Eigen::Matrix3d R_s_t;
 
-  std::vector<double> Q(6, 0.0);
-  Q = rtde_receive.getActualQ();
-  std::cout << Q[0] << " " << Q[1] << " " << Q[2] << " " << Q[3] << " " << Q[4] << " " << Q[5] << std::endl;
+  std::vector<double> InitQ{0, -1.57, -1.57, -1.57, 1.57, 0}; // home:0,-90,0,-90,0,00.0, -1.57, -1.57, -1.57, 1.57, 0
+  rtde_control.moveJ(InitQ);
+  std::vector<double> TCPForce(6, 0.0);
+  TCPForce = rtde_receive.getActualTCPForce();
+  std::cout << "the TCPForce is: " << TCPForce[0] << " " << TCPForce[1] << " " << TCPForce[2] << " " << TCPForce[3]
+            << " " << TCPForce[4] << " " << TCPForce[5] << std::endl;
 
   return 0;
 }
