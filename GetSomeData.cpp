aa44ed2f1cf27d48d3b151d8b62d508f8393e422
likecanyon <1174578375@qq.com>
2022-07-12 10:23:50 +0800

    【测试FT传感器】

--- GetSomeData.cpp
+++ GetSomeData.cpp
@@ -20,9 +20,22 @@ int main(int argc, char *argv[])
   RTDEReceiveInterface rtde_receive("192.168.3.101");
   Eigen::Matrix3d R_s_t;
 
-  std::vector<double> InitQ{0, -1.57, -1.57, -1.57, 1.57, 0}; // home:0,-90,0,-90,0,00.0, -1.57, -1.57, -1.57, 1.57, 0
+  std::vector<double> InitQ{0, -1.57, -1.57, -1.57, 1.57, 0};
+  // home:0,-90,0,-90,0,00.0, -1.57, -1.57, -1.57, 1.57, 0
+  rtde_control.zeroFtSensor();
   rtde_control.moveJ(InitQ);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
   std::vector<double> TCPForce(6, 0.0);
+
+  TCPForce = rtde_receive.getActualTCPForce();
+
+  std::cout << "the TCPForce is: " << TCPForce[0] << " " << TCPForce[1] << " " << TCPForce[2] << " " << TCPForce[3]
+            << " " << TCPForce[4] << " " << TCPForce[5] << std::endl;
+
+  //InitQ[3] = 1.57;
+  rtde_control.moveJ(InitQ);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
   TCPForce = rtde_receive.getActualTCPForce();
   std::cout << "the TCPForce is: " << TCPForce[0] << " " << TCPForce[1] << " " << TCPForce[2] << " " << TCPForce[3]
             << " " << TCPForce[4] << " " << TCPForce[5] << std::endl;
